24cc5a2d31b3a46f88ea48546946c28c9cf3eefd
Andrey Plis <andrey.plis@tibbo.com>
2019-12-31 11:50:25 +0300

    init

---
+++ app/src/main/java/com/andreyplis/recipecounter/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.andreyplis.recipecounter
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.buttonGoods).setOnClickListener { v -> navController.navigate(R.id.action_mainFragment_to_goodsFragment) }
+    }
+
+
+}
