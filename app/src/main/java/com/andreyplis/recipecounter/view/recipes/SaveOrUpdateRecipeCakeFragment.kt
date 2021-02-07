0106ca2b043d59e8f9751249fbd6c94fe659d214
andrey.plis <andrey.plis@tibbo.com>
2021-02-07 10:51:39 +0300

    [upd] updated recipes functionality

--- app/src/main/java/com/andreyplis/recipecounter/view/recipes/SaveOrUpdateRecipeCakeFragment.kt
+++ app/src/main/java/com/andreyplis/recipecounter/view/recipes/SaveOrUpdateRecipeCakeFragment.kt
@@ -3,9 +3,16 @@ package com.andreyplis.recipecounter.view.recipes
 import android.os.*
 import android.view.*
 import androidx.fragment.app.*
-import com.andreyplis.recipecounter.*
+import androidx.navigation.fragment.*
+import com.andreyplis.recipecounter.R
+import com.andreyplis.recipecounter.viewmodel.*
 
 class SaveOrUpdateRecipeCakeFragment : Fragment() {
+
+    private lateinit var viewModel: RecipesViewModel
+
+    private val args: SaveOrUpdateRecipeCakeFragmentArgs by navArgs()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
